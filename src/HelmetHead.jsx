70edc56238ba29b52287e922540598b87707f3fc
Sudhil <sudhil.rk@inapp.com>
2024-05-15 23:28:18 +0530

    Updated SEO content in react helmet head

--- src/HelmetHead.jsx
+++ src/HelmetHead.jsx
@@ -7,9 +7,12 @@ const HelmetHead = () => {
          <title>Music Player</title>
          <meta name='description' content='A simple music player application with React + Vite and Howler.js'/>
          <meta name='keywords' content='Music Player, Sudhil&apos;s Retro Player, srk-music-player'/>
-         <meta property='og:type' content='Website'/>
          <meta property='og:title' content='Music Player'/>
          <meta property='og:description' content='Sudhil&apos;s Retro Player'/>
+         <meta property='og:url' content='https://srk-music-player.netlify.app/'/>
+         <meta property='og:image' content='/logo512.png'/>
+         <meta property='og:type' content='Website'/>
+         <meta property='og:site_name' content='Sudhil&apos;s Retro Player'/>
       </Helmet>
    )
 }
